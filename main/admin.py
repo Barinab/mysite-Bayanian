56e12592f30c317283ce8d6a49a7369e5efcf547
Babak <babak.bayanian@gmail.com>
2023-08-31 19:53:26 +0330

    add admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from main.models import Edu,Exp,Pro
 # Register your models here.
+
+
+admin.site.register(Edu)
+admin.site.register(Exp)
+admin.site.register(Pro)
--- main/models.py
+++ main/models.py
@@ -11,16 +11,16 @@ class Exp(models.Model):
     
 class Edu(models.Model):    
     Edu_nameUni=models.CharField(max_length=30)
-    Edu_date_start=models.CharField(max_length=30)
-    Edu_date_finish=models.CharField(max_length=30)
+    Edu_datestart=models.CharField(max_length=30)
+    Edu_datefinish=models.CharField(max_length=30)
     Edu_majarName=models.CharField(max_length=60)
     Edu_des=models.CharField(max_length=600)
     
     
-class Edu(models.Model):    
+class Pro(models.Model):    
     Pro_name=models.CharField(max_length=30)
-    Pro_Co_name=models.CharField(max_length=30)
-    Pro_date_=models.CharField(max_length=30)
+    Pro_Coname=models.CharField(max_length=30)
+    Pro_date=models.CharField(max_length=30)
     Pro_Subject=models.CharField(max_length=200)
     Pro_des=models.CharField(max_length=600)
 
