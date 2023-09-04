aebf6cfd0ac013b464b08f6a19abe1de5fc0fb2b
Babak <babak.bayanian@gmail.com>
2023-09-04 17:06:32 +0330

    add slug

--- main/models.py
+++ main/models.py
@@ -3,14 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Exp(models.Model):
-    Exp_nameCo=models.CharField(max_length=30)
-    Exp_date_start=models.CharField(max_length=30)
-    Exp_date_finish=models.CharField(max_length=30)
-    Exp_title=models.CharField(max_length=60)
-    Exp_des=models.CharField(max_length=600)
+    def __str__(self) -> str:
+        return self.Exp_nameCo
     
-class Edu(models.Model):    
-    Edu_nameUni=models.CharField(max_length=30)
+    Exp_nameCo=models.CharField(max_length=30,null=True)
+    Exp_date_start=models.CharField(max_length=30,null=True)
+    Exp_date_finish=models.CharField(max_length=30,null=True)
+    Exp_title=models.CharField(max_length=60,null=True)
+    Exp_des=models.CharField(max_length=600,null=True)
+    slug=models.SlugField(null=True)
+    
+class Edu(models.Model):  
+    def __str__(self) -> str:
+        return self.Edu_nameUni
+    Edu_nameUni=models.CharField(max_length=50)
     Edu_datestart=models.CharField(max_length=30)
     Edu_datefinish=models.CharField(max_length=30)
     Edu_majarName=models.CharField(max_length=60)
@@ -18,7 +24,7 @@ class Edu(models.Model):
     
     
 class Pro(models.Model):    
-    Pro_name=models.CharField(max_length=30)
+    Pro_name=models.CharField(max_length=30) 
     Pro_Coname=models.CharField(max_length=30)
     Pro_date=models.CharField(max_length=30)
     Pro_Subject=models.CharField(max_length=200)
@@ -30,4 +36,6 @@ class Service(models.Model):
     Service_date=models.CharField(max_length=30)
     Service_Subject=models.CharField(max_length=200)
     Service_des=models.CharField(max_length=600)
+    
+    
 
--- main/views.py
+++ main/views.py
@@ -2,5 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
+edu_list=[
+    "The University Of Adelaide",
+    "the olum"
+       
+]
+edu_majar=["mechatrinics",
+           "mechanization"]
+
 def home(request):
-    return HttpResponse("home page")
+    context={}
+    return render(request,"main/home.html",context)
+
+
+
+def Edu_show(request):
+    list_of_Edu="<ul>"
+    for j in edu_list:
+        list_of_Edu += f"<li>{j}</li>"
+    list_of_Edu +="</ul>"
+    return HttpResponse(list_of_Edu)
+
+
